426e675e5e1c98297dd4bf3bd443f7208a34bfce
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-03 23:41:47 +0530

    find Max Num

---
+++ MaxNumGen1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace GenericsS
+{
+    public class MaxNumGen1
+    {
+     public static bool Check<T>(T a, T b) 
+        {
+            bool c = a.Equals(b);
+            return c;
+        }
+    }
+    class program 
+    {
+    public static void Main(string[] args)
+        {
+            Console.WriteLine(MaxNumGen1.Check(50,30));
+            Console.ReadLine();
+        }
+        
+    }
+}
