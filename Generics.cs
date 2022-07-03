a62b9cee70c1092ab06792571b7288493cc71b93
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-03 22:16:31 +0530

    Generics

---
+++ Generics.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Write Generics");
+ string Generics = Console.ReadLine();
+string Genericss = "Generics allow to write a class or Method that" +
+                " can work with any data types.";
+Console.WriteLine("Generics :- " + Genericss);
+Console.WriteLine("Generics :- " + "declared by specifying a type parameter in an angle" +
+              " brackets after a type name, e.g. TypeName<T> where T is a type parameter.");
+
