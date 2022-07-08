db3be8ee3cd7120a35b1fdec74d265a06873bcf6
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-09 02:11:15 +0530

    MaxNum

---
+++ GenericsMethodd.cs
@@ -0,0 +1,63 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace GenericsS
+//{
+//    //generic means not specific to a particular data type.
+//    public class GenericsMethodd
+//    {
+
+//        // public static void showarray(int[] arr)
+//        //public static void showarray<T>(T[] arr)
+//        //{
+//        //    for (int i =0; i <arr.Length; i++)
+//        //    {
+//        //        Console.WriteLine(arr[i]);
+//        //    }
+//        //}
+//        ////public static void showarray(string[] arr)
+//       //{
+//        ////    for (int i = 0; i < arr.Length; i++)
+//        ////    {
+//        ////        Console.WriteLine(arr[i]);
+//        ////    }
+//        ////}
+
+//        public static void Add<T>(T a, T b)
+//        {
+           
+
+             
+//        }
+
+//    }
+//    class program1
+//    {
+//       static void Main(string[] args)
+
+//       {
+//            GenericsMethodd.Add(10,20); 
+
+
+//            //GenericsMethodd check = new GenericsMethodd();
+//            // GenericsMethodd obj = new GenericsMethodd();
+
+//            //int[] numbers = new int[3];
+//            //numbers[0] = 20;
+//            //numbers[1]  = 30;
+//            //numbers[2] = 40;
+//            ////Console.WriteLine("The max number is : "+ Math.Max( 10,20));
+//            //string[] name = { "name", "nitish", "Mehta" };
+//            //double[] points = { 4.5, 5.50, 9.9 };
+            
+//            //GenericsMethodd.showarray(numbers);
+//            //GenericsMethodd.showarray(name);
+//            //GenericsMethodd.showarray(points);
+//            Console.ReadLine();
+
+//       }
+//    }
+//}
