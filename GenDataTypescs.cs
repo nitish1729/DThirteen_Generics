6c2005ad585b7df3df2183ab3728efe98db20d10
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-09 02:47:07 +0530

    Check data Tpes by Generic

---
+++ GenDataTypescs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsS
+{
+    public class GenDataTypescs
+    {
+        public static void Check<T>(T a)
+        {
+             // type of method return the data Types
+            Console.WriteLine(typeof(T));
+        }
+    }
+    public class programTwo
+    {
+        static void Main(string[] args)
+        {
+            GenDataTypescs.Check(10);
+        }
+    }
+}
---
+++ MaxNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsS
+{
+   public class MaxNum
+    {
+        public static void Maxx(int a, int b, int c)
+        {
+         if (a > b && a >c)
+            {
+                Console.WriteLine("The Maximum number is : " + a);
+            }
+         else if(b >c && b > a)
+            {
+                Console.WriteLine("The Maximum number is : " + b);
+            }
+         else
+            {
+                Console.WriteLine("The Maximum number is : " + c);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            MaxNum check = new MaxNum();
+            MaxNum.Maxx(10,50,0);
+        }
+    }
+
+}
